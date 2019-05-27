8a8a094faeef1cbfcbc300736ec49dda7a3c16c6
Merel van puymbroeck <merelvanpuymbroeck@gmail.com>
2019-05-27 11:20:45 +0200

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,23 @@
+function mOver(obj) {
+  obj.innerHTML = "Woof Woof!!"
+}
+
+function mOut(obj) {
+  obj.innerHTML = "Doggo"
+}
+
+function showImage(){
+document.getElementById('dogememe').style.visibility="visible";
+}
+
+function showImage(){
+document.getElementById('dogememe').style.visibility= document.getElementById('dogememe').style.visibility == 'visible'? 'hidden' : 'visible';
+}
+
+window.onload = function () {
+    MyFunction()
+}
+
+function MyFunction(){
+    document.getElementById("doge").innerHTML = "Woof!";
+}
